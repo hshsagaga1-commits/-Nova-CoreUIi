e1cbd4373acfb92625f578ecb2849b875d06d6ee
hshsagaga1-commits <hshsagaga1@gmail.com>
2026-09-13 18:30:37 -0300

    Add standalone CoreUI loader

---
+++ Loader.lua
@@ -0,0 +1,14 @@
+local HttpService = game:GetService("HttpService")
+local url = "https://raw.githubusercontent.com/hshsagaga1-commits/-Nova-CoreUIi/main/CoreUI.lua"
+
+local source = game:HttpGet(
+    url .. "?_cb=" .. HttpService:GenerateGUID(false),
+    true
+)
+
+local chunk, err = loadstring(source)
+if not chunk then
+    error(err)
+end
+
+return chunk()
